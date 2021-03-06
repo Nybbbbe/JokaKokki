ad2a46a0ae5dfbbcdccb0bd2c4d427bba4f5bf0d
Carl-Victor Schauman <carl-victor.schauman@aalto.fi>
2021-03-06 16:52:20 +0200

    Fixed scaling issue with login background

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -3,12 +3,9 @@ import './Login.css'
 function Login({userLogin}) {
     return (
         <>
-            <div className="page-title login-title">
-                <h1>Jokakokki</h1>
-            </div>
             <div className="content">
                 <div className="login-container">
-                    <h2 className="login-text"> Kirjaudu sisään: </h2>
+                    <h1 className="login-text"> Jokakokki </h1>
                     <button
                         type="button" 
                         className="btn btn-green" 
