1dea0bfaa1734dcd7a82f37e8c2f3b03a47e8649
Carl-Victor Schauman <cv.schauman@gmail.com>
2021-03-08 00:53:50 +0200

    Added img to userdata, and added the userdata to the profile

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import './Profile.css'
 import VoiceControl from '../../voicecontrol';
 import Switch from '@material-ui/core/Switch';
+import Server from "../../server";
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 
 const Profile = () => {
+    const user = Server.getUser();
     const [checked, setChecked] = React.useState(VoiceControl.voiceControlActive);
     const toggleChecked = () => {
         setChecked((prev) => !prev);
@@ -17,11 +20,16 @@ const Profile = () => {
             <div className="page-title">
                 <h1>Profiili</h1>
             </div>
+            <div className="component-content-container">
+                <h3 className="user-info">Nimi: { user.name }</h3>
+                <h3 className="user-info">Email: { user.email }</h3>
+                <img className="profile-picture" src={ user.img } alt="profile-picture"/>
+            </div>
             <FormGroup>
-            <FormControlLabel
-                control={<Switch checked={checked} onChange={toggleChecked} />}
-                label="Kädetön tila päällä"
-            />
+                <FormControlLabel
+                    control={<Switch checked={checked} onChange={toggleChecked} />}
+                    label="Kädetön tila päällä"
+                />
             </FormGroup>
         </>
     )
