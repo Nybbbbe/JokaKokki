5ee7b575200cf525ebc5bd4755c5670385525c47
Robin Karlsson <robin98.karlsson@gmail.com>
2021-03-16 15:18:55 +0200

    Add suport for local videos

--- src/components/content/Video.js
+++ src/components/content/Video.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactPlayer from 'react-player'
+import React from "react";
+import ReactPlayer from "react-player";
 
-function Video ({src}) {
-    return (
-        <ReactPlayer 
-        url={src} 
-        width="100%"
-        config={{
-            youtube: {
-                playerVars: {
-                    controls: 1,
-                    modestbranding: 1,
-                    loop: 1,
-                    iv_load_policy: 3
-                }
-            },
-        }} 
-        />
-    );
+function Video({ src }) {
+    const config = {
+        youtube: {
+            playerVars: {
+                controls: 1,
+                modestbranding: 1,
+                loop: 1,
+                iv_load_policy: 3
+            }
+        },
+        file: {
+            attributes: {
+                controls: true
+            }
+        }
+    };
 
+    return <ReactPlayer url={src} width="100%" config={config} />;
 }
 
 export default Video;
